b9b88fa4f47d0cac263df3de500687849ef346e7
frank-Regal <fregal@utexas.edu>
2023-07-13 12:43:56 -0500

    repo restructure for plugin only code

--- MsftOpenXRGame/Source/MsftOpenXRGame/MsftOpenXRGame.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2020 Microsoft Corporation.
-// Licensed under the MIT License.
-
-#include "MsftOpenXRGame.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MsftOpenXRGame, "MsftOpenXRGame" );
--- MsftOpenXRGame/Source/MsftOpenXRGame/MsftOpenXRGame.h
+++
@@ -1,7 +0,0 @@
-// Copyright (c) 2020 Microsoft Corporation.
-// Licensed under the MIT License.
-
-#pragma once
-
-#include "CoreMinimal.h"
-
