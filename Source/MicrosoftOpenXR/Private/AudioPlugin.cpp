9d999b69725125cba448143e1c175afdfb12f54f
frank-Regal <fregal@utexas.edu>
2023-11-29 14:02:07 -0600

    audio plugin clean up

--- Source/MicrosoftOpenXR/Private/AudioPlugin.cpp
+++ Source/MicrosoftOpenXR/Private/AudioPlugin.cpp
@@ -1,5 +1,34 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
+// BSD 3-Clause License
+//
+// Copyright (c) 2023, The University of Texas at Austin
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+///////////////////////////////////////////////////////////////////////////////
 
 #include "AudioPlugin.h"
 
@@ -12,16 +41,21 @@ using namespace winrt::Windows;
 
 namespace MicrosoftOpenXR
 {
-	FAudioPlugin::FAudioPlugin()
-	{
+	/*
+	* Constructor
+	*/
+	FAudioPlugin::FAudioPlugin(){}
 
-	}
 
-	FAudioPlugin::~FAudioPlugin()
-	{
+	/*
+	* Destructor
+	*/
+	FAudioPlugin::~FAudioPlugin(){}
 
-	}
 
+	/*
+	* Register this module
+	*/
 	void FAudioPlugin::Register()
 	{
 		//UE_LOG(LogHMD, Warning, TEXT("Audio Plugin Register called."));
@@ -29,19 +63,29 @@ namespace MicrosoftOpenXR
 		FGameDelegates::Get().GetEndPlayMapDelegate().AddRaw(this, &FAudioPlugin::OnEndPlay);
 	}
 
+
+	/*
+	* Unregister this module at game shutdown
+	*/
 	void FAudioPlugin::Unregister()
 	{
 		FGameDelegates::Get().GetEndPlayMapDelegate().RemoveAll(this);
 		IModularFeatures::Get().UnregisterModularFeature(GetModularFeatureName(), this);
 	}
 
-	void FAudioPlugin::OnEndPlay()
-	{
-
-	}
 
 	/*
-	* FIND ALL INTEGRATED DEVICES ON HARDWARE
+	* Called when module is destroyed in game.
+	*/
+	void FAudioPlugin::OnEndPlay(){}
+
+
+	/**
+	* @brief Get all integrated device names and IDs integrated on the HoloLens 2.
+	*
+	* DeviceAdded function gets called when an integrated device is found.
+	* 
+	* @return void		DeviceAdded callback prints to output log the Device ID and Name.
 	*/
 	void FAudioPlugin::GetAllIntegratedDevices()
 	{
@@ -51,6 +95,16 @@ namespace MicrosoftOpenXR
 		UE_LOG(LogTemp, Warning, TEXT("Started to listen for devices"));
 	}
 
+
+	/**
+	* @brief Callback on integrated device found.
+	*
+	* Gets called from GetAllIntegratedDevices()
+	*
+	* @param this        Devices::Enumeration::DeviceInformation watcher object.
+	* @param addedDevice Devices::Enumeration::DeviceInformation information object.
+	* @return void       Prints to output log the Device ID and Name.
+	*/
 	void FAudioPlugin::DeviceAdded(Devices::Enumeration::DeviceWatcher const&, Devices::Enumeration::DeviceInformation const& addedDevice)
 	{
 		// Get and print device information
@@ -59,84 +113,129 @@ namespace MicrosoftOpenXR
 		UE_LOG(LogTemp, Warning, TEXT("Device Name: %s"), addedDevice.Name().c_str());
 	}
 
-	/*
-	 * FIND ONLY INTEGRATED AUDIO INPUT DEVICES (MICROPHONES)
-	 * REF: https://learn.microsoft.com/en-us/azure/ai-services/speech-service/how-to-select-audio-input-devices
-	 */
+
+	/**
+	* @brief Find only integrated audio devices (microphones) on HoloLens 2.
+	* 
+	* reference:
+	* https://learn.microsoft.com/en-us/azure/ai-services/speech-service/how-to-select-audio-input-devices
+	*
+	* @return void		Prints to output log the Device ID and Name.
+	*/
 	void FAudioPlugin::GetIntegratedAudioDevices()
 	{
 		// start async
-		auto promise = Devices::Enumeration::DeviceInformation::FindAllAsync(Devices::Enumeration::DeviceClass::AudioCapture);
+		auto promise = Devices::Enumeration::DeviceInformation::FindAllAsync(
+			Devices::Enumeration::DeviceClass::AudioCapture);
 
 		// callback on async completed
 		promise.Completed([](Foundation::IAsyncOperation<Devices::Enumeration::DeviceInformationCollection> const& sender,
 			Foundation::AsyncStatus /* asyncStatus */) {
 				auto devices = sender.GetResults();
+
+				// log 
 				UE_LOG(LogTemp, Warning, TEXT("Integratred Audio Devices Found:"));
-				for (const auto& device : devices) {
-					UE_LOG(LogTemp, Warning, TEXT("Device Name: %s; ID: %s"), device.Name().c_str(), device.Id().c_str());
+				for (const auto& device : devices) 
+				{
+					// log name and id
+					UE_LOG(LogTemp, Warning, TEXT("Device Name: %s; ID: %s"), 
+						device.Name().c_str(), 
+						device.Id().c_str());
 				}
 			});
 	}
 
-	/*
-	 * CAPTURE RAW AUDIO
-	 * ABOUT: AudioGraph approach. Works using editor.
-	 * TODO: get this working on HoloLens.
-	 * REF: https://github.com/xris1658/cppwinrt-notes/blob/main/AudioGraph-20210617.md
-	 */
+
+	/**
+	* @brief Initializes AudioGraph objects & nodes and registers default audio input device (microphone).
+	*
+	* 1) Creates and initializes (with specific settings) an AudioGraph object, 
+	*    AudioDeviceInputNode object, & FrameOutputNode object. 
+	* 2) Creates a callback that registers the default audio input device (default microphone).
+	* 3) Initializes a RobofleetUnrealClient instance to publish out information from the FrameOutputNode.
+	* 
+	* reference: 
+	* https://github.com/xris1658/cppwinrt-notes/blob/main/AudioGraph-20210617.md
+	*
+	* @return void
+	*/
 	void FAudioPlugin::InitRawAudioCapture()
 	{
-		// AudioGraph creation.
+		// create AudioGraph async operation
 		Media::Audio::AudioGraphSettings settings(Media::Render::AudioRenderCategory::Speech);
 		CreateAudioGraphAsyncOperation = Media::Audio::AudioGraph::CreateAsync(settings);
 		CreateAudioGraphAsyncResult = CreateAudioGraphAsyncOperation.get();
 
-		// Check
+		// error check to ensure audio graph async was successfully created
 		if (CreateAudioGraphAsyncResult.Status() != decltype(CreateAudioGraphAsyncResult.Status())::Success)
 		{
 			UE_LOG(LogTemp, Warning, TEXT("Creating an audio graph "));
 			return;
 		}
 
-		// Declare AudioGraph object
+		// assign async operation tp AudioGraph object
 		MicrophoneAudioGraph = CreateAudioGraphAsyncResult.Graph();
 
-		// OutputNode creation. Create an object that outputs an audio signal to an audio device.
-		//winrt::Windows::Media::MediaProperties::AudioEncodingProperties output_properties;
-		//output_properties.SampleRate(16000);
+		// create OUTPUT node object to latter access raw audio data from
 		AudioOutputNode = MicrophoneAudioGraph.CreateFrameOutputNode();
 
-		// InputNode creation. Create an object that reads an audio signal from an audio device.
-		// TODO: Potentially use other Field: 
-		// ref:  https://learn.microsoft.com/en-us/uwp/api/windows.media.capture.mediacategory?view=winrt-22621
-		//winrt::Windows::Media::MediaProperties::AudioEncodingProperties input_properties;
-		//input_properties.SampleRate(48000);
+		// create INPUT node object that samples data from audio device.
 		CreateAudioDeviceInputNodeOperation = MicrophoneAudioGraph.CreateDeviceInputNodeAsync(
 			Media::Capture::MediaCategory::Speech);
 
-		// CallBack when AudioDevice is found from async operation
+		/* TODO: set output properties
+		winrt::Windows::Media::MediaProperties::AudioEncodingProperties output_properties;
+		output_properties.SampleRate(16000);
+		AudioOutputNode = MicrophoneAudioGraph.CreateFrameOutputNode(output_properties);
+		*/
+
+		/* TODO: set input properties
+		// ref:  https://learn.microsoft.com/en-us/uwp/api/windows.media.capture.mediacategory?view=winrt-22621
+		winrt::Windows::Media::MediaProperties::AudioEncodingProperties input_properties;
+		input_properties.SampleRate(48000);
+		CreateAudioDeviceInputNodeOperation = MicrophoneAudioGraph.CreateDeviceInputNodeAsync(
+			Media::Capture::MediaCategory::Speech, input_properties);
+		*/
+
+		// async operation. when default audio input device is found, set as input node device. 
 		CreateAudioDeviceInputNodeOperation.Completed([this](
 			Foundation::IAsyncOperation<Media::Audio::CreateAudioDeviceInputNodeResult> const& results,
 			auto&& async_status)
 			{
-				// check
+				// check for successful discovery
 				if (results.get().Status() != Media::Audio::AudioDeviceNodeCreationStatus::Success)
 				{
 					UE_LOG(LogTemp, Warning, TEXT("failure attempting to access audio input device"));
 					return;
 				}
 
-				// Create audio input node for graph and start capturing the audio
+				// assign device to audio input node
 				AudioInputNode = results.get().DeviceInputNode();
+
+				// add output node connection
 				AudioInputNode.AddOutgoingConnection(AudioOutputNode);
+
+				// start data capture by starting audio graph and input node
 				StartRawAudioCapture();
 			});
 
+		// create RobofleetUnrealClient module object for later publishing
 		robofleet_instance_ = FRobofleetUnrealClientModule::Get()->RobofleetClient;
 	}
 
-	// called initially from audio graph init function
+
+	/**
+	* @brief Starts AudioGraph and audio input nodes to start capturing data from audio device (microphone).
+	*
+	* Called from inside the InitRawAudioCapture() method. InitRawAudioCapture() needs to be
+	* called before this. QuantumStarted callback inside this function is called continuously
+	* when microphone data is begin sampled on the audio device.
+	* 
+	* reference:
+	* https://github.com/xris1658/cppwinrt-notes/blob/main/AudioGraph-20210617.md
+	*
+	* @return void
+	*/
 	void FAudioPlugin::StartRawAudioCapture()
 	{
 		MicrophoneAudioGraph.Start();      // first start graph
@@ -156,6 +255,12 @@ namespace MicrosoftOpenXR
 			});
 	}
 
+
+	/**
+	* @brief Logs AudioInputNode encoding properties and publishes information to Robofleet.
+	*
+	* @return void Print to log.
+	*/
 	void FAudioPlugin::GetAudioGraphEncodingProperties()
 	{
 		Media::MediaProperties::AudioEncodingProperties encoding_properties = AudioInputNode.EncodingProperties();
@@ -167,64 +272,65 @@ namespace MicrosoftOpenXR
 		UE_LOG(LogTemp, Warning, TEXT("bitrate: %d bits/s"), encoding_properties.Bitrate());
 		UE_LOG(LogTemp, Warning, TEXT("bit depth / bits per sample: %d bits per sample"), encoding_properties.BitsPerSample());
 
-		// create struct
-		/*AudioInfo audio_info;
+		return; // DEBUG: don't publish to robofleet.
+
+		// create AudioInfo struct to publish over robofleet
+		AudioInfo audio_info;
 		audio_info.channels = encoding_properties.ChannelCount();
 		audio_info.sample_rate = encoding_properties.SampleRate();
 		audio_info.sample_format = "S32LE";
 		audio_info.bitrate = encoding_properties.Bitrate();
-		audio_info.coding_format = "wave";*/
+		audio_info.coding_format = "wave";
 
-		// Publish
-		//FRobofleetUnrealClientModule::Get()->RobofleetClient->PublishAudioInfo("microphone/info", "hololens", audio_info);
+		// publish to robofleet
+		FRobofleetUnrealClientModule::Get()->RobofleetClient->PublishAudioInfo("microphone/info", "hololens", audio_info);
 	}
 
+
+	/**
+	* @brief Access the raw audio data in the AudioGraph OutputNode and publish over Robofleet.
+	*
+	* Called from inside the StartRawAudioCapture() QuantumStarted(...) callback.
+	* Where all the magic happens for capturing raw audio data.
+	*
+	* @return void Publishes information to RobofleetServer.
+	*/
 	void FAudioPlugin::GetRawAudioCaptureData()
 	{
 		// init
 		uint8_t* dataInBytes{ nullptr };
 		uint32_t capacityInBytes{ 0U };
 
+		// get frame from audio output node and lock the buffer
 		Media::AudioFrame audio_frame = AudioOutputNode.GetFrame();
-		Media::AudioBuffer buffer = audio_frame.LockBuffer(
-			Media::AudioBufferAccessMode::Read);
-		Foundation::IMemoryBufferReference reference = buffer.CreateReference(); // ref: https://learn.microsoft.com/en-us/uwp/api/windows.media.audiobuffer.createreference?view=winrt-22621#windows-media-audiobuffer-createreference
+		Media::AudioBuffer buffer = audio_frame.LockBuffer(Media::AudioBufferAccessMode::Read);
+
+		// ref: https://learn.microsoft.com/en-us/uwp/api/windows.media.audiobuffer.createreference?view=winrt-22621#windows-media-audiobuffer-createreference
+		Foundation::IMemoryBufferReference reference = buffer.CreateReference(); 
 		
-		// fill data
+		// fill byte data from buffer into array
 		auto byteAccess = reference.as<IMemoryBufferByteAccess>();
 		winrt::check_hresult(byteAccess->GetBuffer(&dataInBytes, &capacityInBytes));
 
-		// fill data
+		// format and fill audio data into vector RobofleetUnrealClient can publish.
 		audio_data_.data.clear();
 		audio_data_.data.resize(capacityInBytes);
 		memcpy(audio_data_.data.data(), dataInBytes, capacityInBytes);
 
-		// publish over robofleet
+		// publish to robofleet server
 		FRobofleetUnrealClientModule::Get()->RobofleetClient->PublishAudioData("microphone/data", "hololens", audio_data_);
-
-		// DEBUG: to remove
-		// NumCaptures++;
 	}
 
+	/**
+	* @brief Stop AudioGraph and stop accessing microphone
+	*
+	* @return void
+	*/
 	void FAudioPlugin::StopRawAudioCapture()
 	{
-		AudioInputNode.Stop();       // stop audio input node
-
-		//// DEBUG: remove
-		//stop_time = std::chrono::high_resolution_clock::now();
-
-		MicrophoneAudioGraph.Stop(); // stop AudioGraph object
-
-		//// DEBUG: remove
-		//duration = std::chrono::duration_cast<std::chrono::milliseconds>(stop_time - start_time);
-		//float DurationInSeconds = static_cast<float>(duration.count()) / 1000.0f;
-		//UE_LOG(LogTemp, Warning, TEXT("Audio Capture Stopped"));
-		//UE_LOG(LogTemp, Warning, TEXT("Time elapsed: %f s"), DurationInSeconds);
-		//UE_LOG(LogTemp, Warning, TEXT("Num Audio Messages: %d"), NumCaptures);
+		AudioInputNode.Stop();       
+		MicrophoneAudioGraph.Stop();
 	}
-
-
-
 
 } // namespace MicrosoftOpenXR
 
--- Source/MicrosoftOpenXR/Private/AudioPlugin.h
+++ Source/MicrosoftOpenXR/Private/AudioPlugin.h
@@ -1,10 +1,38 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// BSD 3-Clause License
+//
+// Copyright (c) 2023, The University of Texas at Austin
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// * Redistributions of source code must retain the above copyright notice, this
+//   list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+//
+// * Neither the name of the copyright holder nor the names of its
+//   contributors may be used to endorse or promote products derived from
+//   this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+///////////////////////////////////////////////////////////////////////////////
 
 #pragma once
 
 #if PLATFORM_WINDOWS || PLATFORM_HOLOLENS
-
-#include <chrono>
 
 #include "OpenXRCommon.h"
 #include "OpenXRCore.h"
@@ -50,49 +78,53 @@
 
 using namespace winrt::Windows;
 
-/**
- * 
- */
 namespace MicrosoftOpenXR
 {
-	// this is used to create audio
+	/**
+	* @brief Used to access audio buffer
+	*/ 
 	struct __declspec(uuid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")) __declspec(novtable) IMemoryBufferByteAccess : ::IUnknown
 	{
 		virtual HRESULT __stdcall GetBuffer(std::uint8_t** value, std::uint32_t* capacity) = 0;
 	};
 
+	/**
+	* @brief Plugin class used to access raw audio (microphone) data on HoloLens 2 
+	*        or any other windows based device from Unreal Engine application.
+	*/
 	class FAudioPlugin : public IOpenXRExtensionPlugin
 	{
 	public:
+
+		// audio plugin setup methods
 		FAudioPlugin();
 		~FAudioPlugin();
-
 		void Register();
 		void Unregister();
 		void OnEndPlay();
 
-		//
+		// access integrated devices on HoloLens 2
 		void GetAllIntegratedDevices();
 		void DeviceAdded(Devices::Enumeration::DeviceWatcher const& , Devices::Enumeration::DeviceInformation const& addedDevice);
 		void GetIntegratedAudioDevices();
 
-		//
+		// methods required to access raw audio data from HoloLens 2 microphone
 		void InitRawAudioCapture();
 		void StartRawAudioCapture();
 		void GetRawAudioCaptureData();
 		void StopRawAudioCapture();
 
+		// log function for audio encoding properties
 		void GetAudioGraphEncodingProperties();
 
 	private:
 
-		// integrated device objects
+		// integrated device watcher
 		Devices::Enumeration::DeviceWatcher HoloLensDeviceWatcher{ nullptr };
 		
-		// async processes
+		// async process objects
 		Foundation::IAsyncOperation<Media::Audio::CreateAudioGraphResult> CreateAudioGraphAsyncOperation{ nullptr };
 		Foundation::IAsyncOperation<Media::Audio::CreateAudioDeviceInputNodeResult> CreateAudioDeviceInputNodeOperation{ nullptr };
-		
 		Media::Audio::CreateAudioGraphResult CreateAudioGraphAsyncResult{ nullptr };
 		Media::Audio::CreateAudioDeviceInputNodeResult CreateAudioDeviceInputNodeAsyncResult{ nullptr };
 
@@ -101,18 +133,10 @@ namespace MicrosoftOpenXR
 		Media::Audio::AudioDeviceInputNode AudioInputNode{ nullptr };
 		Media::Audio::AudioFrameOutputNode AudioOutputNode{ nullptr };
 
-		// DEBUG: profiling audio capture
-		std::chrono::high_resolution_clock::time_point start_time;
-		std::chrono::high_resolution_clock::time_point stop_time;
-		std::chrono::milliseconds duration{ 0 };
-		bool StartAudioCapture = false;
-		int NumCaptures{ 0 };
-
-		// publish to robofleet
+		// objects required to publish on robofleet
 		AudioData audio_data_;
 		URobofleetBase* robofleet_instance_;
 		AudioDataStamped audio_data_stamped_;
-
 	};
 
 }
